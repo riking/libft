83e4ff73700ffd681a1b657914aed9a29d6a57b6
Kane York <kyork@student.42.us.org>
2016-10-17 21:49:51 -0700

    change alloc_wrap to fprintf(stderr)

--- alloc_wrap.c
+++ alloc_wrap.c
@@ -6,7 +6,7 @@
 /*   By: kyork <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/18 22:04:36 by kyork             #+#    #+#             */
-/*   Updated: 2016/10/11 11:23:52 by kyork            ###   ########.fr       */
+/*   Updated: 2016/10/17 14:55:05 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <ft_alloc_wrap.h>
+/*
+** This library is neither thread-safe nor fork-safe.
+**
+** g_alloc_count provides a counter of the number of live allocations.
+** You could check its value before and after a function call, for example.
+** TODO: provide a way to turn off the atexit() print
+**
+**   extern int g_alloc_count;
+**
+** g_malloc_inject is used to cause malloc() to return NULL. When it is set to
+** 0, the next allocation will return NULL and g_malloc_inject will be set to
+** -1. When it is greater than 0, malloc will behave normally and the counter
+** will be decremented.
+**
+**   extern int g_malloc_inject;
+**
+** It would be very stupid to turn this file in with your work, especially if
+** it is included in the default compile configuration.
+*/
 
 int			g_alloc_count = 0;
 int			g_malloc_inject = -1;
@@ -24,9 +42,9 @@ static int	g_have_registered = 0;
 static void	print_alloc_count(void)
 {
 	if (g_alloc_count != 0)
-		printf("FAIL: %d mallocs not freed\n", g_alloc_count);
+		fprintf(stderr, "FAIL: %d mallocs not freed\n", g_alloc_count);
 	else
-		printf("[OK]: all allocations freed\n");
+		fprintf(stderr, "[OK]: all allocations freed\n");
 }
 
 void		*malloc(size_t sz)
@@ -42,14 +60,14 @@ void		*malloc(size_t sz)
 	if (g_malloc_inject >= 0)
 		if (g_malloc_inject-- == 0)
 		{
-			printf("malloc(%ld) = INJECT\n", sz);
+			fprintf(stderr, "malloc(%ld) = INJECT\n", sz);
 			return (0);
 		}
 	libc_malloc = dlsym(RTLD_NEXT, "malloc");
 	addr_alloc = libc_malloc(sz);
 	if (addr_alloc)
 		g_alloc_count++;
-	printf("malloc(%ld) = %p\n", sz, addr_alloc);
+	fprintf(stderr, "malloc(%ld) = %p\n", sz, addr_alloc);
 	return (addr_alloc);
 }
 
@@ -58,7 +76,7 @@ void		free(void *p)
 	void	(*libc_free)(void*);
 
 	libc_free = dlsym(RTLD_NEXT, "free");
-	printf("free(%p)\n", p);
+	fprintf(stderr, "free(%p)\n", p);
 	libc_free(p);
 	if (p)
 		g_alloc_count--;
