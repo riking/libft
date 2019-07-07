0f6e7dfdad1c015caa5c941f43d0f388592a33ee
Kane York <kanepyork@google.com>
2019-07-06 20:06:49 -0700

    Make internal libft.h includes consistent

--- ft_strtab_destroy.c
+++ ft_strtab_destroy.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 #include <stdlib.h>
 
 void	ft_strtab_destroy(char **tab)
